1023ee548dcc3d7873084e819c81b9383b553f3e
Isaac <isaacjpoulin@gmail.com>
2020-02-08 12:09:10 -0600

    Initial commit

---
+++ webproject-ex4-0210-poulin/src/util/Info.java
@@ -0,0 +1,7 @@
+package util;
+
+public interface Info {
+   public String projectName = "webproject-ex4-0210-poulin";
+   public String searchWebName = "simpleSearchHB.html";
+   public String insertWebName = "simpleInsertHB.html";
+}
